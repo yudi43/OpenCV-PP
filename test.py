9c8366281e88be88ebf20d9ea741ed57e7880537
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-16 06:14:37 +0530

    reading videos and showing them

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import cv2
 
+cap = cv2.VideoCapture('resources/test.mp4')
 
-img = cv2.imread('resources/test.jpeg')
-
-cv2.imshow('output', img)
-cv2.waitKey(0)
+print(cap)
+while(True):
+   success, img = cap.read()
+   cv2.imshow('Video', img)
+   if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
